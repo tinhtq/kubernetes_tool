272ab53810dc8a28e5641f652e2cfcb207c11c85
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-05-20 17:28:39 +0700

    update telnet function

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-
+from app.telnet import telnet_session
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/telnet")
+async def handle_telnet(host: str, port: int, username: str, password: str, command: str):
+    try:
+        session = telnet_session(host, port)
+        return {"result": "hello"}
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/hello/{name}")
